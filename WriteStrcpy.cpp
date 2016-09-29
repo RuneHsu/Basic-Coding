81429ec19a3e9e9f94c4c4db9b0122de3bc38926
RuneHsu <RuneHsu@users.noreply.github.com>
2016-09-29 09:44:11 +0800

    Create WriteStrcpy.cpp

---
+++ WriteStrcpy.cpp
@@ -0,0 +1,10 @@
+char *strcpy(char *strDest, const char *strSrc)
+{
+  if (strDest == NULL || strSrc == NULL)
+    return NULL;
+  if (srcDest == strSrc)
+    return strDest;
+  char *tmp = strDest;
+  while ((*strDest++ = *strSrc++) != '\0');
+  return temp;
+}
